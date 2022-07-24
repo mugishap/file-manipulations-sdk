388c2a0b306e6e24b1c0a9c94625e6c5bd5cd316
mugishap <precieuxmugisha@gmail.com>
2022-07-24 22:31:34 +0200

    Will add uploader tomorrow

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,23 +1,53 @@
 const XLSX = require('xlsx');
-const {parseExcel} = require('./../utils/parser')
+const { parseExcel } = require('./../utils/parser')
+const path = require('path')
+const connection = require('./../models/database')
 
 exports.getJsonFromDatabase = async (req, res) => {
+    try {
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: error.message })
+    }
 }
 exports.uploadExcelFile = async (req, res) => {
+    try {
 
-    //Upload file using multer
-
-    parseExcel(__basedir+'/uploads/'+req.file.filename).forEach(element => {
-        console.log(element.data);
-    });
-
+        //Upload file using multer
+        parseExcel(__basedir + '/uploads/' + req.file.filename).forEach(element => {
+            console.log(element.data);
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: error.message })
+    }
 }
-exports.getAllTableData = async (req, res) => {
 
+exports.getAllTableData = async (req, res) => {
+    try {
+        const query = 'SELECT * FROM items;'
+        connection.query(query, function (err, rows) {
+            if (err) {
+                console.log("The error is here")
+                return res.status(200).json({ error: err })
+            } else {
+                console.log(rows)
+                return res.status(200).json({rows})
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 exports.home = async (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    try {
+        return res.status(200).sendFile(path.resolve('views/index.html'))
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: error.message })
+    }
 }
 
