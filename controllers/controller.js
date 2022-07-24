e02dbb6936a568998a0049d737ada7745730b38c
mugishap <precieuxmugisha@gmail.com>
2022-07-24 21:44:55 +0200

    Adding excel reader packages

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,27 @@
+const XLSX = require('xlsx');
 
-
-exports.getJsonFromDatabase = async(req,res)=>{
-
-}
-exports.uploadExcelFile = async(req,res)=>{
+exports.getJsonFromDatabase = async (req, res) => {
 
 }
-exports.getAllTableData = async(req,res)=>{
+exports.uploadExcelFile = async (req, res) => {
+
+
+    const parseExcel = (filename) => {
+
+        const excelData = XLSX.readFile(filename);
+
+        return Object.keys(excelData.Sheets).map(name => ({
+            name,
+            data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
+        }));
+    };
+
+    parseExcel("./ss.xls").forEach(element => {
+        console.log(element.data);
+    });
+
+}
+exports.getAllTableData = async (req, res) => {
 
 }
 
